c8aecb934fcb81a848ca85cdbf0fc0904386fb99
Brayden Wilmoth <brayden.wilmoth@gmail.com>
2025-05-22 20:34:18 -0400

    Initial commit

--- example/src/index.ts
+++ example/src/index.ts
@@ -1,37 +1,30 @@
 import { Actor, handler, fetchActor, Worker, ActorState, getActor } from '../../packages/core/src'
 
-// Worker extend WorkerEntrypoint           <-- DONE
-// Binding to a DO from another Worker
-// What does this look like with RPC        <-- DONE
-// What do websockets look like
-// Remove executeTransaction                <-- DONE
-
-// 1 - The entrypoint starts here..
+// Example worker with RPC call into actor
 export default class MyWorker extends Worker<Env> {
     async fetch(request: Request): Promise<Response> {
-        // return new Response('Worker')
-        
-        const actor2 = MyActor2.get('default');
-        const total = await actor2?.customTestFunc(400, 200);
-
+        const actor = MyActor.get('default');
+        const total = await actor?.add(400, 200);
         return new Response(`Worker Count: ${total}`)
-        // return fetchActor(request, MyActor)
     }
 }
 
-// 2 - Which then calls this first Actor..
+// Example actor with RPC function
 export class MyActor extends Actor<Env> {
     constructor(state: ActorState, env: Env) {
         super(state, env);
     }
 
     async fetch(request: Request): Promise<Response> {
-        // return new Response('Actor')
         return fetchActor(request, MyActor2)
+    }
+
+    public async add(a: number, b: number): Promise<number> {
+        return a + b;
     }
 }
 
-// 3 - Which can also call into this next Actor...
+// Example actor with idFromRequest and database querying
 export class MyActor2 extends Actor<Env> {
     static idFromRequest(request: Request): string {
         // Path should follow the pattern `/user/:id` and we should use the correct Actor instance
@@ -40,24 +33,16 @@ export class MyActor2 extends Actor<Env> {
         return pathParts.length === 3 ? pathParts[2] : "default";
     }
 
-    constructor(state: ActorState, env: Env) {
-        super(state, env);
-    }
-
     async fetch(request: Request): Promise<Response> {
-        const result = await this.database.executeQuery({ sql: 'SELECT 4+5;', isRaw: true })
-        return new Response(`Actor ${MyActor2.idFromRequest(request)} - ${JSON.stringify(result)}`);
-    }
-
-    public customTestFunc(a: number, b: number): number {
-        return a + b;
+        const result = await this.database.executeQuery({ sql: 'SELECT 4+5;' })
+        return new Response(`Error in Actor - ${MyActor2.idFromRequest(request)} - ${JSON.stringify(result)}`);
     }
 }
 
 // You can tell your incoming request to route to your Worker
 // export default handler(MyWorker); 
 
-// Try to skip the Worker and go direct to the Actor
+// Try to skip the Worker and go direct to an Actor
 // export default handler(MyActor); 
 // export default handler(MyActor2); 
 
